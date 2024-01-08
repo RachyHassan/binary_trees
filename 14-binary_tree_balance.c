b4705fffa225a00bfc1717d40f38544a8faee451
RachyHassan <tolulopehassan97@gmail.com>
2024-01-08 13:12:48 +0100

    adjustments

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: A pointer to the root node
- * @Return: If tree is NULL - 0
+ * Return: If tree is NULL - 0
  * Otherwise, return the balace factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
@@ -12,6 +12,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	return (0);
 }
+
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -22,12 +23,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
-		{
-			size_t l = 0, r = 0;
-
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height(tree->right): 1;
-			return ((l > r) ? l : r);
-		}
+	{
+		size_t l = 0, r = 0;
+		
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right): 1;
+		return ((l > r) ? l : r);
+	}
 	return (0);
 }
